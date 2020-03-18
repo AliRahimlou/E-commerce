5200fe394d1b3a6a07c29ced4cb4c3d2271f7507
Ali Rahimlou <Rahimlou91@gmail.com>
2020-03-18 15:33:33 -0400

    set up irtem preview page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import React from 'react';
 import{Switch, Route} from 'react-router-dom'
 
 import './App.css';
-import HomePage from '../src/pages/homepage/homepage'
+import HomePage from './pages/homepage/homepage'
+import ShopPage from './pages/shop/shop'
 
-const HatsPage =()=>(
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/shop/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
